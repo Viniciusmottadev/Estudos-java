14361b99fc7cc4437661d8246c25e2b1f44338ad
Vinícius Motta <vini9motta@gmail.com>
2026-07-16 08:37:23 -0300

    Primeiro teste em Java
    Neste commit apenas adicionei minha primeira interação com o java

---
+++ firstprogram.java
@@ -0,0 +1,21 @@
+package maratonajava.introducao;
+//isso é um comentario de linha
+/*
+isso é um comentario
+de multiplas
+linhas
+ */
+/**
+ * Isso é um comentario <b>JAVA DOC</b>
+ */
+public class firstprogram {
+    public static void main (String[] args){
+        System.out.println("My mama called: Seen you on TV, son");
+        System.out.println("Said shit done changed ever since we was on");
+        System.out.println("I dreamed it all ever since I was young");
+        System.out.println("They said I wouldn't be nothing");
+        System.out.println("Now they always say congratulations");
+        System.out.println("ALGO");
+
+    }
+}
