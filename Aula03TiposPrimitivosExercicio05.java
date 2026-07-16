d6362151747be18a3364aa9285f0c055cc2760c2
Vinícius Motta <vini9motta@gmail.com>
2026-07-16 12:41:50 -0300

    Exercício Tipos Primitivos - 05

---
+++ Aula03TiposPrimitivosExercicio05.java
@@ -0,0 +1,20 @@
+package maratonajava.introducao;
+/*
+Prática 4
+
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+O aluno <nome> está matriculado no curso <curso>,
+estuda no período <período> e possui matrícula <matrícula>.
+*/
+public class Aula03TiposPrimitivosExercicio05 {
+    public static void main(String[] args) {
+        String nomeAluno ="Vitor";
+        String curso="Desenvolvimento de sistemas";
+        String periodoEscola ="Diurno";
+        long matricula =55243;
+
+        System.out.println("O aluno "+nomeAluno+" está matriculado no curso de "+curso+" estuda no período "+periodoEscola+" e possui matrícula "+matricula);
+
+    }
+}
