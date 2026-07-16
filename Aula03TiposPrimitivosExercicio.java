12cbf7822e7b7b5754db92198c7ae22a9e8ae081
Vinícius Motta <vini9motta@gmail.com>
2026-07-16 09:23:21 -0300

    Exercício usando Tipos Primitivos
    Neste commit eu fiz um simples exercício de prática utilizando tipos primitivos (String e double).

---
+++ Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,21 @@
+package maratonajava.introducao;
+/*
+Prática
+
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço>,
+confirmo que recebi o salário de <salário>, na data <data>
+ */
+public class Aula03TiposPrimitivosExercicio {
+    public static void main (String[] args){
+        String nome = "Marcos";
+        String endereco = "Rua das palmeiras";
+        double salario = 8580.90;
+        String dataRecebimentoSalario = "16/07/2026";
+        String relatorio = "Eu "+nome+", morando no endereço "+endereco+" confirmo que recebi o salário de "+salario+", na data "+dataRecebimentoSalario;
+
+        System.out.println(relatorio);
+
+    }
+}
