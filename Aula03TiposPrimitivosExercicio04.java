b47603bf0a7a690ec5e5d6d0cedd9b52ec577d16
Vinícius Motta <vini9motta@gmail.com>
2026-07-16 12:31:09 -0300

    Exercício Tipos Primitivos - 04
    Neste commit eu salvei o exercício de tipos primitivos java (String, int e double).

---
+++ Aula03TiposPrimitivosExercicio04.java
@@ -0,0 +1,18 @@
+package maratonajava.introducao;
+/*
+Prática 3
+
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+O filme <filme> foi lançado em <ano>,
+possui nota <nota> e pertence ao gênero <gênero>.
+*/
+public class Aula03TiposPrimitivosExercicio04 {
+    public static void main(String[] args){
+        String nomeFilme ="Toy Story 5";
+        int anoLancamento = 2026;
+        double notaFilme= 5.1;
+        String generoFilme="Comédia";
+        System.out.println("O filme "+nomeFilme+" foi lançado em "+anoLancamento+" possui nota "+notaFilme+" e pertence ao gênero "+generoFilme);
+    }
+}
