41818f0f30f9e25d006383909b96d5405a1d1c59
Vinícius Motta <vini9motta@gmail.com>
2026-07-16 08:38:51 -0300

    Aula 02 do curso
    Neste commit estou adicionando minha segunda aula e primeira interação com tipos primitivos.

---
+++ Aula02TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    static void main(String[] args){
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("A idade de paulo é de " + idade);
+        System.out.println(falso);
+        System.out.println("char " + caractere);
+    }
+}
