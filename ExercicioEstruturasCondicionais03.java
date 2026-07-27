66b661219c4031ada68b171f85beabcd4b5eabd5
Vinícius Motta <vini9motta@gmail.com>
2026-07-27 16:02:23 -0300

    Exercício Estrutura Condicional - 03 (Switch, Case)

--- ExercicioEstruturasCondicionais03.java
+++ ExercicioEstruturasCondicionais03.java
@@ -1,29 +1,43 @@
 package maratonajava.introducao;
 /*
-Prática 3
+Prática 1
 
 Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
 
-O produto <produto> possui <quantidade> unidades em estoque.
+O funcionário <nome> pertence ao departamento de código <codigoDepartamento>.
 
-Se a quantidade for maior que 0, imprima:
+Utilize switch para exibir o departamento correspondente.
 
-"Produto disponível."
+1 - Recursos Humanos
+2 - Desenvolvimento
+3 - Financeiro
+4 - Marketing
 
-Caso contrário, imprima:
+Caso o código não exista, exiba:
 
-"Produto indisponível."
+"Departamento inválido."
 */
 public class ExercicioEstruturasCondicionais03 {
     public static void main(String[] args) {
-        String nomeProduto = "Sabonete ";
-        int quantidadeProduto = 267;
-        System.out.println("O produto "+nomeProduto+" possui "+quantidadeProduto+" unidades em estoque.");
+    String nomeFunc = "Nicole";
+    byte codDepartamento = 4;
+        System.out.println("O(a) funcionário(a) "+nomeFunc+" pertence ao departamento de código "+codDepartamento);
 
-        if(quantidadeProduto > 0){
-            System.out.println("Produto disponível.");
-        }else {
-            System.out.println("Produto indisponível.");
+        switch (codDepartamento){
+            case 1:
+                System.out.println("Recursos Humanos");
+                break;
+            case 2:
+                System.out.println("Desenvolvimento");
+                break;
+            case 3:
+                System.out.println("Financeiro");
+                break;
+            case 4:
+                System.out.println("Marketing");
+                break;
+            default:
+                System.out.println("Departamento inválido");
         }
     }
 }
